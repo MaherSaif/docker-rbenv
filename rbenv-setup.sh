11bbd6385d7058b4e04bcaa3b469b901c9899cda
Viktor Vad <vad.viktor@gmail.com>
2015-04-20 10:58:45 +0100

    persisting rbenv bootstrap

--- rbenv-setup.sh
+++ rbenv-setup.sh
@@ -2,6 +2,8 @@
 
 git clone https://github.com/sstephenson/rbenv.git /root/.rbenv && \
 git clone https://github.com/sstephenson/ruby-build.git /root/.rbenv/plugins/ruby-build && \
+echo 'PATH="/root/.rbenv/bin:$PATH"' | tee -a /root/.bashrc && \
+echo 'eval "$(rbenv init -)"' | tee -a /root/.bashrc && \
 export PATH="/root/.rbenv/bin:$PATH" && \
 eval "$(rbenv init -)" && \
 rbenv install $1 && \
