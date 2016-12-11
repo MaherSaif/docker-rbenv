b01c4f6158c9fdc145bc46fa9d2327196f48fa46
Viktor (Icon) VAD <vad.viktor@gmail.com>
2016-12-11 18:20:31 +0000

    adding optional rbenv speedup

--- scripts/rbenv-setup.sh
+++ scripts/rbenv-setup.sh
@@ -4,6 +4,13 @@ git clone --depth 1 https://github.com/sstephenson/rbenv.git /root/.rbenv && \
 git clone --depth 1 https://github.com/sstephenson/ruby-build.git /root/.rbenv/plugins/ruby-build && \
 rm -rfv /root/.rbenv/plugins/ruby-build/.git && \
 rm -rfv /root/.rbenv/.git && \
+
+# Optionally, try to compile dynamic bash extension to speed up rbenv. Don't worry if it fails; rbenv will still work normally:
+cd /root/.rbenv && \
+src/configure && \
+make -C src && \
+# ###
+
 export PATH="/root/.rbenv/bin:$PATH" && \
 export RUBY_CFLAGS='-O2' && \
 export CONFIGURE_OPTS="--disable-install-doc" && \
